7885ff462b39255da53676c3fc4025a6afa91d78
rcereno <rcereno@calpoly.edu>
2021-01-14 11:55:53 -0800

    updated perm_lex files

--- perm_lex.py
+++ perm_lex.py
@@ -1,3 +1,20 @@
-def perm_gen_lex(a): 
+# string --> string(s)
+def perm_gen_lex(a):
+    if len(a) == 1:
+        return [a]
+    if a == '':
+        return []
+    list = []
+    for char in a:
+        keep = char
+        newStr = a.replace(char, '')
+        reduced_result = perm_gen_lex(newStr)
+        for i in reduced_result:
+            list.append(keep + i)
+#        list.append(keep + str(reduced_result))
+    return list
+
+
+
 
  
--- perm_lex_testcases.py
+++ perm_lex_testcases.py
@@ -6,5 +6,25 @@ class TestAssign1(unittest.TestCase):
     def test_perm_gen_lex(self):
         self.assertEqual(perm_lex.perm_gen_lex('ab'),['ab','ba'])
 
+    def test_perm_gen_lex02(self):
+        self.assertEqual(perm_lex.perm_gen_lex('abc'), ['abc', 'acb', 'bac', 'bca', 'cab', 'cba'])
+
+    def test_perm_gen_lex03(self):
+        self.assertEqual(perm_lex.perm_gen_lex(''), [])
+
+    def test_perm_gen_lex04(self):
+        self.assertEqual(perm_lex.perm_gen_lex('r'), ['r'])
+
+    def test_perm_gen_lex05(self):
+        self.assertEqual(perm_lex.perm_gen_lex('l'), ['l'])
+
+    def test_perm_gen_lex06(self):
+        self.assertEqual(perm_lex.perm_gen_lex('art'), ['art', 'atr', 'rat', 'rta', 'tar', 'tra'])
+
+#    def test_perm_gen_lex07(self):
+
+#    def test_perm_gen_lex03(self):
+#        self.assertEqual(perm_lex.perm_gen_lex('bob'), ['bob', 'bbo', 'obb', 'obb', 'bbo', 'bob'])
+
 if __name__ == "__main__":
         unittest.main()
